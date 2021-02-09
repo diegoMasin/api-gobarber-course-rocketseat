41574b8daebfb53609c3002704296385b8218dc5
Diego Masin <diegoifce@gmail.com>
2021-02-09 17:43:29 -0300

    Linkando o Model ao Migration.

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,23 @@
-import { v4 } from 'uuid';
+// import { v4 } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column() // Sem argumento default varchar
   provider: string;
 
+  @Column('time with time zone') // Se não estiver utilizando postgres, não terá suporte ao "with time zone"
   date: Date;
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = v4();
-    this.provider = provider;
-    this.date = date;
-  }
+  // A função do constructor está sendo subjugada pelo TypeORM.
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = v4();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
 }
 
 export default Appointment;
