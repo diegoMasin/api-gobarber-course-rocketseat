c9906d7c5d2e226767bc72a9cb3d601142af6586
Diego Masin <diegoifce@gmail.com>
2021-02-09 16:42:46 -0300

    Criando primeira Migration.

--- src/database/migrations/1612814350296-CreateAppointments.ts
+++ src/database/migrations/1612814350296-CreateAppointments.ts
@@ -1,7 +1,33 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1612814350296 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+export default class CreateAppointments1612814350296 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar', // Varchar por que usaremos UUID
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
 }
