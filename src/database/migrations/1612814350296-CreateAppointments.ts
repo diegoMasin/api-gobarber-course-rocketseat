a02b33050a0bb8f5b74e9cf1638217283c7bf969
Diego Masin <diegoifce@gmail.com>
2021-02-09 18:24:10 -0300

    É preciso não só dizer que é uuid, precisa gerar.

--- src/database/migrations/1612814350296-CreateAppointments.ts
+++ src/database/migrations/1612814350296-CreateAppointments.ts
@@ -11,6 +11,7 @@ export default class CreateAppointments1612814350296 implements MigrationInterfa
             type: 'varchar', // Varchar por que usaremos UUID
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
